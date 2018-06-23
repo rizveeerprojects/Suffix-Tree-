b753c245a96f0d30008ee1f286591fe1b32b49f6
rizveeerprojects <37921783+rizveeerprojects@users.noreply.github.com>
2018-06-24 02:15:38 +0600

    Update brutforce.cpp

--- brutforce.cpp
+++ brutforce.cpp
@@ -3,12 +3,12 @@ using namespace std;
 #define MAX 500
 #define MAX_SKIP 3
 
-//bruteforce code to generate valid pattern
-
 map<string,int>M;
 map<int,string>M_rev; 
 int cnt;
 vector<int>occurence_vector[MAX+1];
+map<char,double>weight;
+double minimumSupportThreshhold;
 
 void generate(string s,int idx, int start, int end,int skip_cnt,int max_skip,vector<char>V){
 	if(idx>end) {
@@ -58,8 +58,9 @@ void generate(string s,int idx, int start, int end,int skip_cnt,int max_skip,vec
 		V.pop_back();
 	}
 	return;
-	
 }
+
+
 
 void func(string s){
 	cnt=0; 
@@ -76,10 +77,37 @@ void func(string s){
 		occurence_vector[M[temp]].push_back(i);
 		generate(s,i+1,i,s.size()-1,0,MAX_SKIP,V);
 	}
-	cout<<cnt<<endl;
+	weight['a']=0.8;
+	weight['b']=0.6;
+	weight['c']=0.7;
+	weight['d']=0.5;
+	weight['$']=0.0001;
+	minimumSupportThreshhold=0.2;
+	int guni=0;
 	for(int i=1;i<=cnt;i++){
-		cout<<M_rev[i]<<endl;
+		string temp = M_rev[i];
+		double wt=0;
+		int len=0;
+		for(int j=0;j<(int)temp.size();j++){
+			if(temp[j] != '*') {
+				wt = wt + weight[temp[j]];
+				len++; 
+			}
+		}
+		if(len != 0) {
+			wt /= len;
+		}
+		//cout<<" value " << wt <<" "<< occurence_vector[M[temp]].size()<<endl;
+		if(wt*occurence_vector[M[temp]].size() >= minimumSupportThreshhold) {
+			guni++;
+			cout<<temp<<" "<<guni<<endl;
+		}
+		/*for(int j=0;j<(int)occurence_vector[M[temp]].size();j++) {
+			cout<<occurence_vector[M[temp]][j]<<" ";
+		}*/
+		//cout<<endl;
 	}
+	
 }
 
 
